78ea081d22473989bfc057c0791b6f349c1b0e23
glad432 <120590157+glad432@users.noreply.github.com>
2024-04-12 18:31:56 +0530

    main

--- src/Components/ReadonlyEditor.js
+++ src/Components/ReadonlyEditor.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Editor from '@monaco-editor/react';
 
 const ReadonlyEditor = ({ content, darkMode }) => {
+  const Minifiededitor = useRef(null);
+
   const linesCount = content.split('\n').length;
   const charactersCount = content.length;
+
+  const handleEditorDidMount = (editor, monaco) => {
+    Minifiededitor.current = editor;
+  };
+
+  const handleCopyContent = async () => {
+    if (Minifiededitor.current) {
+      await navigator.clipboard.writeText(Minifiededitor.current.getValue());
+      const lastLineNumber = Minifiededitor.current.getModel().getLineCount();
+      Minifiededitor.current.revealLine(lastLineNumber);
+      Minifiededitor.current.setSelection({
+        startLineNumber: 1,
+        startColumn: 1,
+        endLineNumber: lastLineNumber,
+        endColumn: Minifiededitor.current.getModel().getLineMaxColumn(lastLineNumber),
+      });
+    }
+  };
 
   return (
     <div>
@@ -32,9 +52,12 @@ const ReadonlyEditor = ({ content, darkMode }) => {
           cursorSmoothCaretAnimation: true,
           cursorStyle: 'line',
         }}
+        onMount={handleEditorDidMount}
       />
+      <button className='px-4 py-2 mt-2 font-bold cursor-pointer bg-green-500 rounded text-white hover:bg-green-600 ease-out overflow-hidden transform md:hover:scale-x-105 md:hover:scale-y-100' onClick={handleCopyContent}>Copy</button>
     </div>
+    
   );
-}
+};
 
 export default ReadonlyEditor;
